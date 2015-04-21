f306753d9ad301a5ee024e70782d23712f4a553d
Pete Young <gnuoy.etep@gmail.com>
2015-04-21 17:43:02 -0500

    ch02 print/1 and either_or_both/2

--- ch02/my_module.erl
+++ ch02/my_module.erl
@@ -2,7 +2,20 @@
 
 -module(my_module).
 
--export([pie/0]).
+-export([pie/0,
+         print/1,
+         either_or_both/2]).
 
 pie() ->
     3.14.
+
+print(Term) ->
+    io:format("The value of Term is: ~p.~n", [Term]).
+    %io:format("The value of Term is: ~w.~n", [Term]).
+
+either_or_both(true, B) when is_boolean(B) ->
+    true;
+either_or_both(A, true) when is_boolean(A) ->
+    true;
+either_or_both(false, false) ->
+    false.
